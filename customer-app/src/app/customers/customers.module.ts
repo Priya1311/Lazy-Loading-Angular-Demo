fb1c6b39757dd7db91667338f9604d91c5c97d95
Priya Gupta <priya@ruckit.me>
2018-11-06 13:44:53 +0530

    create customer module and customer module routing files

---
+++ customer-app/src/app/customers/customers.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomersRoutingModule } from './customers-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CustomersRoutingModule
+  ]
+})
+export class CustomersModule { }
