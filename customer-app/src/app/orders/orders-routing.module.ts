2d643f9b958305810e358de946e42acb20d609ad
Priya Gupta <priya@ruckit.me>
2018-11-06 13:50:38 +0530

    configure order component route

--- customer-app/src/app/orders/orders-routing.module.ts
+++ customer-app/src/app/orders/orders-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { OrderListComponent } from './order-list/order-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: OrderListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
