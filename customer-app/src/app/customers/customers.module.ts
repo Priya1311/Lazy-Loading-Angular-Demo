48a2a31f6fb6a7319d81aaa2930097888da9ac90
Priya Gupta <priya@ruckit.me>
2018-11-06 13:45:40 +0530

    add customer component

--- customer-app/src/app/customers/customers.module.ts
+++ customer-app/src/app/customers/customers.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CustomerListComponent],
   imports: [
     CommonModule,
     CustomersRoutingModule
