c96960a244d8d1a7f2d1b29501acab57e0d40707
Priya Gupta <priya@ruckit.me>
2018-11-06 13:46:57 +0530

    create order-list component

--- customer-app/src/app/orders/orders.module.ts
+++ customer-app/src/app/orders/orders.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrdersRoutingModule } from './orders-routing.module';
+import { OrderListComponent } from './order-list/order-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [OrderListComponent],
   imports: [
     CommonModule,
     OrdersRoutingModule
